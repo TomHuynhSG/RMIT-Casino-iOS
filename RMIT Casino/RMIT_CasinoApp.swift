772b33f21b459db7ad1eebad73a90617fe991f66
Tom Huynh <tomhuynhsg@gmail.com>
2022-08-01 21:22:15 +0700

    Initial Commit

---
+++ RMIT Casino/RMIT_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_CasinoApp.swift
+//  RMIT Casino
+//
+//  Created by Tom Huynh on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
