f7b8069e71da32d9d7c7ded2fc5f6c2a7052fa14
Tom Huynh <tomhuynhsg@gmail.com>
2022-08-02 11:44:57 +0700

    Improve visibility of x button for InfoView

--- RMIT Casino/Views/InfoView.swift
+++ RMIT Casino/Views/InfoView.swift
@@ -11,55 +11,64 @@ struct InfoView: View {
   @Environment(\.presentationMode) var presentationMode
   
   var body: some View {
-    VStack(alignment: .center, spacing: 10) {
-      LogoView()
-      
-      Spacer()
-      
-      Form {
-        Section(header: Text("How to play")) {
-            Text("Just spin the reels to play.")
-            Text("Matching all icons to win.")
-            Text("The winning amount will be 10x of your betting amount.")
-            Text("You can reset the money and highscore by clicking on the button Reset.")
-        }
-          Section(header: Text("About the application")) {
-              HStack {
-                Text("App Name")
-                Spacer()
-                Text("RMIT Casino")
+      ZStack{
+          Color("ColorBlueRMIT")
+          VStack(alignment: .center, spacing: 10) {
+            LogoView()
+            
+            Spacer()
+            
+            Form {
+              Section(header: Text("How to play")) {
+                  Text("Just spin the reels to play.")
+                  Text("Matching all icons to win.")
+                  Text("The winning amount will be 10x of your betting amount.")
+                  Text("You can reset the money and highscore by clicking on the button Reset.")
               }
-              HStack {
-                Text("Year Published")
-                Spacer()
-                Text("2022")
+                Section(header: Text("About the application")) {
+                    HStack {
+                      Text("App Name")
+                      Spacer()
+                      Text("RMIT Casino")
+                    }
+                    HStack {
+                      Text("Course")
+                      Spacer()
+                      Text("COSC2659")
+                    }
+                    HStack {
+                      Text("Year Published")
+                      Spacer()
+                      Text("2022")
+                    }
+                    HStack {
+                      Text("Location")
+                      Spacer()
+                      Text("Saigon South Campus")
+                    }
               }
-              HStack {
-                Text("Location")
-                Spacer()
-                Text("Saigon South Campus")
-              }
-        }
+            }
+            .font(.system(.body, design: .rounded))
+          }
+          .padding(.top, 40)
+          .overlay(
+            Button(action: {
+              audioPlayer?.stop()
+              self.presentationMode.wrappedValue.dismiss()
+            }) {
+              Image(systemName: "xmark.circle")
+                .font(.title)
+            }
+            .foregroundColor(.white)
+            .padding(.top, 30)
+            .padding(.trailing, 20),
+            alignment: .topTrailing
+            )
+            .onAppear(perform: {
+              playSound(sound: "drum-music", type: "mp3")
+            })
       }
-      .font(.system(.body, design: .rounded))
-    }
-    .padding(.top, 40)
-    .overlay(
-      Button(action: {
-        audioPlayer?.stop()
-        self.presentationMode.wrappedValue.dismiss()
-      }) {
-        Image(systemName: "xmark.circle")
-          .font(.title)
-      }
-      .padding(.top, 30)
-      .padding(.trailing, 20)
-      .accentColor(Color.secondary)
-      , alignment: .topTrailing
-      )
-      .onAppear(perform: {
-        playSound(sound: "drum-music", type: "mp3")
-      })
+    
   }
 }
 
